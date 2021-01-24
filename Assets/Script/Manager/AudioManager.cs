183eb92176e631923fd0bd763eb191d7357eb145
yilmaz13 <ahhmet13yilmaz@gmail.com>
2021-01-25 01:07:43 +0300

    feat: add audio manager

---
+++ Assets/Script/Manager/AudioManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    public AudioSource AudioSource;
+    public AudioClip BackGroundClip;
+
+    public void PlayBackGroundClip()
+    {
+        AudioSource.PlayOneShot(BackGroundClip);
+    }
+}
