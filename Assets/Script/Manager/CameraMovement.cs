e840b838ba50f7776439a6e0f1199d120d0c6a96
yilmaz13 <ahhmet13yilmaz@gmail.com>
2021-01-25 01:08:10 +0300

    feat: add camera manager

---
+++ Assets/Script/Manager/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour
+{
+    // The target we are following
+    public Transform target;
+
+    void LateUpdate()
+    {
+        if (!target) return;
+
+        transform.position = target.position;
+        transform.position = new Vector3(transform.position.x, transform.position.y + 14, transform.position.z - 11);
+    }
+
+    public void SetTarget(Transform target)
+    {
+        this.target = target;
+    }
+}
